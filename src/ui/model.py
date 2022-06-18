6cd36784adb9d50e88f7796dcd2d15efe2f3f16c
petrioski <petri.karhunkoski@gmail.com>
2022-06-18 22:12:12 +0300

    user defined parameters for game, improved looping

--- src/ui/model.py
+++ src/ui/model.py
@@ -1,17 +1,56 @@
 from resources.instructions import Instructions
+from ui.reader import Selection_reader
 from engine.game import RockPaperScissors
 from engine.player import Player
 
 
 class ui:
     def __init__(self):
-        pass
+        self.ins = Instructions()
+        self.sel = Selection_reader()
 
     def start(self):
-        plr1 = Player("Human", False)
-        plr2 = Player("Computer", True, 2)
-        rps = RockPaperScissors(plr1, plr2)
-        ins = Instructions()
-        ins.welcome()
-        ins.game_instructions()
-        rps.play_game()
+        """
+        Starts looping the game and allows user to play several games without
+        restarting the program
+        """
+        self.ins.welcome()
+        play_again = True
+        while play_again:
+            interrupted = self.play()
+            if not interrupted:
+                play_again = self.sel.play_again()
+            else:
+                break
+
+    def play(self) -> bool:
+        """
+        Creates instance of game. User can choose to play agains computers or
+        simulate computer versus computer game with different Markov chain
+        levels
+
+        Returns:
+            bool: True if user wants to exit game, False otherwise
+        """
+        self.ins.game_instructions()
+        game_mode = self.sel.read_play_mode_input()
+
+        if game_mode == 0:
+            return True
+
+        rounds = self.sel.read_round_input()
+
+        if game_mode == 1:
+            plr1 = Player("Player 1", False)
+            p2_name = "Computer"
+        else:
+            p1_name = "Computer 1"
+            lvl = self.sel.read_markov_level(p1_name)
+            plr1 = Player(p1_name, True, lvl)
+            p2_name = "Computer 2"
+
+        p2_lvl = self.sel.read_markov_level(p2_name)
+        plr2 = Player(p2_name, True, p2_lvl)
+        rps = RockPaperScissors(plr1, plr2, rounds)
+
+        return rps.play_game()
--- src/ui/reader.py
+++ src/ui/reader.py
@@ -1,10 +1,54 @@
-# from engine.selections import RPS_object as rps
-
-
 class Selection_reader:
-    def __init__(self) -> None:
-        # self.read_input()
-        pass
+    def read_round_input(self) -> int:
+        answer = ""
+        while not self.is_valid_number(answer):
+            answer = input("How many rounds: ")
+
+        return int(answer)
+
+    def is_valid_number(self, answer: int, min: int = 1) -> bool:
+        try:
+            rounds = int(answer)
+            if rounds >= min:
+                return True
+        except ValueError:
+            return False
+
+    def read_play_mode_input(self) -> int:
+        answer = -1
+        while answer < 0:
+            response = input(
+                "Select game mode: \n"
+                "\t1 - You play against computer \n"
+                "\t2 - Simulation, computer vs. computer\n"
+                "\tQ - Quit\n"
+            )
+
+            if response.upper() == "Q":
+                return 0
+
+            if self.is_valid_number(response):
+                if int(response) in [1, 2]:
+                    answer = int(response)
+
+        return answer
+
+    def read_markov_level(self, name: str) -> int:
+        answer = -1
+        while answer < 0:
+            response = input(f"Select Markov chain level for {name}: ")
+            if self.is_valid_number(response, 0):
+                if int(response) >= 1 or int(response) <= 10:
+                    answer = int(response)
+        return answer
+
+    def play_again(self) -> bool:
+        while True:
+            res = input("\n\nPlay again? (Y/N): ")
+            if res.upper() == "Y":
+                return True
+            elif res.upper() == "N":
+                return False
 
     def read_input(self) -> str:
 
