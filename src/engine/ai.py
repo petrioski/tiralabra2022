f5e85dfde393323899ac539dca9c783c0379a69b
petrioski <petri.karhunkoski@gmail.com>
2022-06-21 23:22:27 +0300

    multiAi class added

--- src/engine/ai.py
+++ src/engine/ai.py
@@ -58,6 +58,9 @@ class markovRPS(SimpleRPS):
         self.prev_choice = None
         self.choices = self.__create_matrix()
         self.losing_object = Rules().losing_obj
+        self.winning_object = Rules().winning_obj
+        self.results = list()
+        self.chosen = None
 
     def __create_matrix(self) -> dict:
         """Initializes choices dictionary"""
@@ -79,6 +82,7 @@ class markovRPS(SimpleRPS):
         if self.observation_count >= self.history_len:
             self.choices[self.prev_choice][next] += 1
         self._add_observation(next)
+        self._add_points(next)
 
     def _add_observation(self, next: rps):
         self.observation_count += 1
@@ -86,9 +90,23 @@ class markovRPS(SimpleRPS):
         min_obs = min(self.history_len, len(self.obs_list))
         self.prev_choice = tuple(self.obs_list[-min_obs:])
 
-    def get_object(self):
+    def _add_points(self, next: rps):
+        points = 0
+        if self.chosen in self.losing_object[next]:
+            points = 1
+        elif self.chosen in self.winning_object[next]:
+            points = -1
+        self.results.append(points)
+
+    def get_points(self, last_n: int) -> int:
+        if len(self.results) == 0:
+            return 0
+        return sum(self.results[-last_n:])
+
+    def get_object(self) -> rps:
         random_choice = super().get_object()
         if self.observation_count < self.history_len:
+            self.chosen = random_choice
             return random_choice
         else:
             history = self.choices.get(self.prev_choice)
@@ -100,4 +118,48 @@ class markovRPS(SimpleRPS):
 
         winning_space = self.losing_object[next_choice]
 
-        return random.choice(list(winning_space))
+        self.chosen = random.choice(list(winning_space))
+        return self.chosen
+
+
+class multiAi(SimpleRPS):
+    def __init__(
+        self, max_degree: int = 5, focus_length: int = 3, show_stats=False
+    ) -> None:
+        super().__init__()
+        # self.observation_count = 0
+        self.max_dg = max_degree
+        self.fl = focus_length
+        self.show_stats = show_stats
+        # init mv chains 1 - 5
+        self.mcs = dict()
+        for i in range(0, self.max_dg):
+            self.mcs[i] = markovRPS(i + 1)
+
+    def get_object(self) -> rps:
+        best_streak = -1_000_000_000
+        best_ai = 0
+
+        for i in range(0, self.max_dg):
+            next_ai = self.mcs.get(i)
+
+            points = next_ai.get_points(self.fl)
+            obj = next_ai.get_object()
+            if self.show_stats:
+                print(
+                    f">>> ai #{i} - points {points} - object chosen {obj.name}"
+                )
+            if points > best_streak:
+                best_ai = i
+                best_obj = obj
+                best_streak = points
+        if self.show_stats:
+            print(f"ai #{best_ai} chosen")
+        return best_obj
+
+    def update(self, next: rps):
+        # self.observation_count += 1
+        for i in range(0, self.max_dg):
+            next_ai = self.mcs.get(i)
+            next_ai.update(next)
+            self.mcs[i] = next_ai
