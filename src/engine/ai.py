96001996b105a8fd8d548b5e92ff6116ca47a082
petrioski <petri.karhunkoski@gmail.com>
2022-06-30 17:14:52 +0300

    docstrings

--- src/engine/ai.py
+++ src/engine/ai.py
@@ -12,11 +12,25 @@ class SimpleRPS:
             list of non-playable help items
         objects : list
             list of playable game objects.
+        losing_object : dict
+            dictionary of game rules, where RPS_object key gets list of game
+            objects that beat the key
+        winning_object : dict
+            dictionary of game rules, where RPS_object key gets list of game
+            objects that loses to the key
+        chosen : RPS_object
+            Latest chosen object. Gets updated when get_object method is called
+        results : list
+            list of won or lost points per round
     """
 
     def __init__(self) -> None:
         self.help_items = [rps("Q"), rps("H")]
         self.objects = list(rps)
+        self.losing_object = Rules().losing_obj
+        self.winning_object = Rules().winning_obj
+        self.results = list()
+        self.chosen = None
         for i in self.help_items:
             self.objects.remove(i)
 
@@ -26,28 +40,66 @@ class SimpleRPS:
         Returns:
             rps: RPS_object
         """
-        return random.choice(self.objects)
+        self.chosen = random.choice(self.objects)
+        return self.chosen
 
-    def update(self, next: rps):
-        pass
+    def _add_points(self, next: rps) -> None:
+        """
+        Records points from previous round by comparing opponent's choice to
+        own previous choise.
+
+        Args:
+            next (RPS_object): previous selection by opponent
+        """
+        points = 0
+        if self.chosen in self.losing_object[next]:
+            points = 1
+        elif self.chosen in self.winning_object[next]:
+            points = -1
+        self.results.append(points)
+
+    def get_points(self, last_n: int) -> int:
+        """
+        Returns the total points per rounds limited by parameter
+
+        Args:
+            last_n (int): How many previous rounds to be summed up
+
+        Returns:
+            int: sum of points
+        """
+        if len(self.results) == 0:
+            return 0
+        return sum(self.results[-last_n:])
+
+    def update(self, next: rps) -> None:
+        """
+        Updates history data with opponent's previous selection
+
+        Args:
+            next (RPS_object): Opponent's previous selection
+        """
+        self._add_points(next)
 
 
 class markovRPS(SimpleRPS):
-    """_summary_
+    """
+    Ai player based on Markov chain. Predicts next move based on opponent's
+    past selections
 
     Args:
-        SimpleRPS (_type_): _description_
+        SimpleRPS : Inherits the basic methods from naive ai
 
     Attributes:
         observations : int
-            number of opponent selections observed
+            number of opponent selections observed, defaults to 1
         prev_choice : RPS_object
-            opponent's latest selection
-        losing_object : dict
-            dictionary of game rules, where RPS_object key gets list of game
-            objects that beat the key
+            opponent's latest object played
         choices : dict
-            to do: change this to Markov matrix
+            Probability matrix based on past observations
+        history_len : int
+            Markov chain degree, indicates how many past observations are
+            used to evaluate next move
     """
 
     def __init__(self, history_length: int = 1) -> None:
@@ -55,15 +107,11 @@ class markovRPS(SimpleRPS):
         self.history_len = history_length
         self.observation_count = 0
         self.obs_list = list()
-        self.prev_choice = None
         self.choices = self.__create_matrix()
-        self.losing_object = Rules().losing_obj
-        self.winning_object = Rules().winning_obj
-        self.results = list()
-        self.chosen = None
 
     def __create_matrix(self) -> dict:
-        """Initializes choices dictionary"""
+        """Initializes choices dictionary, which is used as probability
+        matrix"""
         choices = dict()
         key_space = product(self.objects, repeat=self.history_len)
         for p in key_space:
@@ -78,32 +126,41 @@ class markovRPS(SimpleRPS):
 
         return choices
 
-    def update(self, next: rps):
+    def update(self, next: rps) -> None:
+        """
+        Updates history data with opponent's previous selection.
+        Overrides inherited method with extra features
+
+        Args:
+            next (RPS_object): Opponent's previous selection
+        """
         if self.observation_count >= self.history_len:
             self.choices[self.prev_choice][next] += 1
         self._add_observation(next)
         self._add_points(next)
 
-    def _add_observation(self, next: rps):
+    def _add_observation(self, next: rps) -> None:
+        """
+        Stores data of the past opponent's selection. Keeps count of rounds
+        played.
+
+        Args:
+            next (RPS_object): Opponent's previous selection
+        """
         self.observation_count += 1
         self.obs_list.append(next)
         min_obs = min(self.history_len, len(self.obs_list))
         self.prev_choice = tuple(self.obs_list[-min_obs:])
 
-    def _add_points(self, next: rps):
-        points = 0
-        if self.chosen in self.losing_object[next]:
-            points = 1
-        elif self.chosen in self.winning_object[next]:
-            points = -1
-        self.results.append(points)
-
-    def get_points(self, last_n: int) -> int:
-        if len(self.results) == 0:
-            return 0
-        return sum(self.results[-last_n:])
-
     def get_object(self) -> rps:
+        """
+        Initialized prediction by making a random selection. If sufficient
+        history available, then uses transition matrix to make actual
+        prediction
+
+        Returns:
+            RPS_object: prediction of next winning object
+        """
         random_choice = super().get_object()
         if self.observation_count < self.history_len:
             self.chosen = random_choice
@@ -123,6 +180,24 @@ class markovRPS(SimpleRPS):
 
 
 class multiAi(SimpleRPS):
+    """
+    Collection of Markov chains of different degrees.
+
+    Args:
+        SimpleRPS : Inherits the basic methods from naive ai
+
+    Attributes:
+        max_dg : int
+            Highest degree of Markov chain to be included to multiAI
+        fl : int
+            Focus length, number of past rounds to evaluate when
+            judging best performing model
+        show_stats : bool
+            True prints out selection process of best performing model and
+            total points in focus range of each ai.
+        mcs : dict
+            Dictonary including different models
+    """
     def __init__(
         self, max_degree: int = 5, focus_length: int = 3, show_stats=False
     ) -> None:
@@ -131,10 +206,22 @@ class multiAi(SimpleRPS):
         self.fl = focus_length
         self.show_stats = show_stats
         self.mcs = dict()
+
         for i in range(0, self.max_dg):
-            self.mcs[i] = markovRPS(i + 1)
+            self.mcs[i] = markovRPS(history_length=i + 1)
+
+        self.mcs[self.max_dg] = SimpleRPS()
+        self.max_dg += 1
 
     def get_object(self) -> rps:
+        """
+        Loops through different models and select prediction from the one with
+        highest points. If multiple models with equal points chooses the
+        simplest (one with lowest index)
+
+        Returns:
+            RPS_object: prediction of next winning object
+        """
         best_streak = -1_000_000_000
         best_ai = 0
 
@@ -152,10 +239,17 @@ class multiAi(SimpleRPS):
                 best_obj = obj
                 best_streak = points
         if self.show_stats:
-            print(f"ai #{best_ai} chosen")
+            print(f"> ai #{best_ai} chosen")
         return best_obj
 
     def update(self, next: rps):
+        """
+        Updates history data with opponent's previous selection.
+        Overrides inherited method with extra features
+
+        Args:
+            next (RPS_object): Opponent's previous selection
+        """
         for i in range(0, self.max_dg):
             next_ai = self.mcs.get(i)
             next_ai.update(next)
